cce7dfb834be2bea67593c0e67f59c656743032b
Joshua Adams <joshadams554@gmail.com>
2018-10-17 16:35:50 -0400

    restarted with correct branch........ also set up folders table and created folders route

--- db/noteful.sql
+++ db/noteful.sql
@@ -1,4 +1,17 @@
--- psql -U dev -d noteful-app -f ./db/noteful.sql
+-- psql -U josh -d noteful-app -f ./db/noteful.sql
+DROP TABLE IF EXISTS folders;
+
+CREATE TABLE folders (
+    id serial PRIMARY KEY,
+    name text NOT NULL
+);
+ALTER SEQUENCE folders_id_seq RESTART WITH 100;
+
+INSERT INTO folders (name) VALUES
+  ('Archive'),
+  ('Drafts'),
+  ('Personal'),
+  ('Work');
 
 DROP TABLE IF EXISTS notes;
 
@@ -6,10 +19,18 @@ CREATE TABLE notes (
   id serial PRIMARY KEY,
   title text NOT NULL,
   content text,
-  created timestamp DEFAULT now()
+  created timestamp DEFAULT now(),
+  folder_id int REFERENCES folders(id) ON DELETE SET NULL
 );
 
 ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
+
+INSERT INTO notes (title, content, folder_id) VALUES
+  (
+    '5 life lessons learned from cats',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    100
+  );
 
 INSERT INTO notes (title, content) VALUES
   (
