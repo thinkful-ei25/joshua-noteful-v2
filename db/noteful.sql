6999d8b794b166a7efc0cc2561e9de842189ab68
Joshua Adams <joshadams554@gmail.com>
2018-10-18 14:49:19 -0400

    added tags, and tags_folders to DB and tags to routes

--- db/noteful.sql
+++ db/noteful.sql
@@ -1,13 +1,23 @@
--- psql -U dev -d noteful2-app -f ./db/noteful.sql
-
+-- psql -U josh -d noteful2-app -f ./db/noteful.sql
+DROP TABLE IF EXISTS notes_tags;
+DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS notes;
+
 DROP TABLE IF EXISTS folders;
 
 CREATE TABLE folders (
     id serial PRIMARY KEY,
-    name text NOT NULL UNIQUE
+    name text NOT NULL
 );
 ALTER SEQUENCE folders_id_seq RESTART WITH 100;
+
+INSERT INTO folders (name) VALUES
+  ('Archive'),
+  ('Drafts'),
+  ('Personal'),
+  ('Work');
+
+
 
 CREATE TABLE notes (
   id serial PRIMARY KEY,
@@ -19,91 +29,101 @@ CREATE TABLE notes (
 
 ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
 
--- If you delete a folder then set folder_id to null on related notes
--- IOW, delete a folder and move the notes to "uncategorized"
--- ALTER TABLE notes ADD COLUMN folder_id int REFERENCES folders(id) ON DELETE SET NULL;
+CREATE TABLE tags (
+  id serial PRIMARY KEY,
+  name text UNIQUE
+);
 
--- Prevent folders from being deleted if are referenced by any note
--- IOW, only empty folder can be deleted
--- ALTER TABLE notes ADD COLUMN folder_id int REFERENCES folders(id) ON DELETE RESTRICT;
+ALTER SEQUENCE tags_id_seq RESTART WITH 500;
 
--- If you delete a folder then delete all notes that reference the folder
--- IOW, delete a folder and all the notes in it
--- ALTER TABLE notes ADD COLUMN folder_id int REFERENCES folders(id) ON DELETE CASCADE;
-
-INSERT INTO folders (name) VALUES
-  ('Archive'),
-  ('Drafts'),
-  ('Personal'),
-  ('Work');
+CREATE TABLE notes_tags(
+  note_id INTEGER NOT NULL REFERENCES notes ON DELETE CASCADE,
+  tag_id INTEGER NOT NULL REFERENCES tags ON DELETE CASCADE
+);
 
 INSERT INTO notes (title, content, folder_id) VALUES
   (
-    '5 life lessons learned from cats',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    , 101
+    'The most boring article about cats you''ll ever read',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    102
   ),
+
   (
     'What the government doesn''t want you to know about cats',
-    'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.'
-  , 101
+    'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
+    100
   ),
   (
-    'The most boring article about cats you''ll ever read',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  , 102
-  ),
+    '5 life lessons learned from cats',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    100
+  );
+
+INSERT INTO notes (title, content) VALUES
+  
   (
     '7 things lady gaga has in common with cats',
     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.'
-  , 103
   ),
   (
     'The most incredible article about cats you''ll ever read',
     'Lorem ipsum dolor sit amet, boring consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  , 100
   ),
   (
     '10 ways cats can help you live to 100',
     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.'
-  , NULL
   ),
   (
     '9 reasons you can blame the recession on cats',
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  , 101
   ),
   (
     '10 ways marketers are making you addicted to cats',
     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.'
-  , 101
   ),
   (
     '11 ways investing in cats can make you a millionaire',
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    , NULL
-  ),
-  (
-    'This is a test for the DB',
-    'Pay no attention to the man behind the curtain',
-    100
   ),
   (
     'Why you should forget everything you learned about cats',
     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.'
-  , NULL
+  );
+
+  INSERT INTO tags(name) VALUES
+  (
+    'cats'
+  ),
+  (
+    'zebras'
+  ),
+  (
+    'orangutans'
+  );
+
+  -- tag = 500 note = 1000
+
+  INSERT INTO notes_tags(note_id, tag_id) VALUES
+  (
+    1000,
+    500
+  ),
+  (
+    1000,
+    501
+  ),
+  (
+    1001,
+    500
+  ),
+  (
+    1003,
+    502
+  ),
+  (
+    1003,
+    501
   );
 
 -- -- get all notes
--- SELECT * FROM notes;
-
--- -- get all folders
--- SELECT * FROM folders;
-
--- -- get all notes with folders
--- SELECT * FROM notes
--- INNER JOIN folders ON notes.folder_id = folders.id;
-
--- -- get all notes, show folders if they exists otherwise null
--- SELECT folder_id as folderId FROM notes
--- LEFT JOIN folders ON notes.folder_id = folders.id;
+-- SELECT title FROM notes;
